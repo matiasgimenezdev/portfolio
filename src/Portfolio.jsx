7e724ff5c5c65e6c5df63093aa25a18791832559
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-02-21 16:24:06 -0300

    adding ToastContainer

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,9 +1,20 @@
-import { Header } from './components/Header/Header';
+import { ToastContainer, Zoom } from 'react-toastify';
+import { Header, Body } from './components';
 
 export function Portfolio() {
 	return (
 		<div>
 			<Header />
+			<Body />
+			<ToastContainer
+				limit={3}
+				position='bottom-right'
+				autoClose={4000}
+				pauseOnHover={false}
+				closeOnClick={true}
+				theme='colored'
+				transition={Zoom}
+			/>
 		</div>
 	);
 }
