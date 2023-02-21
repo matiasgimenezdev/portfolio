d264bce2d31e7054b42df1870e95697b36e53e91
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-02-21 16:27:48 -0300

    starting with the Body

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,11 @@
+import './Body.scss';
+
+export function Body() {
+	return (
+		<>
+			<img src='/coding.gif' alt='coding' />
+			<img src='/developer.gif' alt='developer' />
+			<img src='/dev.gif' alt='dev' />
+		</>
+	);
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './Header.scss';
-import Contact from '../Contact/Contact';
-import logo from '../../assets/logo.png';
+import { Contact } from '../';
 
 export function Header() {
 	return (
 		<header className='header'>
 			<section className='title-container'>
-				<img src={logo} alt='logo' className='logo' />
+				<img src='/logo.png' alt='logo' className='logo' />
 				<h1 className='title'>
 					<span className='underline'>Matías Giménez</span>
 				</h1>
