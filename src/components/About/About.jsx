f7e62dee0fb1f96912cfd62e9d9e5681d0a0bca2
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-02-22 18:59:45 -0300

    fixing responsive fonts

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,18 +18,15 @@ export function About() {
 					<Link href='https://www.unlu.edu.ar/' value='UNLu' /> y me
 					encuentro finalizando el tercer año de la carrera.
 					<br />
-					<br />
 					Me considero un apasionado de la tecnologia y el desarrollo
 					de software, por lo cual trato de seguir capacitandome día a
 					día. En este momento me encuentro en el proceso de aprender
 					React para poder agregarlo a mi stack de tecnologias.
 					<br />
-					<br />
 					Estoy buscando la oportunidad de combinar mi formacion
 					academica con el trabajo, con el objetivo de desarrollar
 					conocimientos y potenciar los adquiridos durante mis
 					estudios.
-					<br />
 					<br />
 					<span className='location'>
 						<ImLocation />
