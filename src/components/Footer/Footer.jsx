7bf6e8efdc7729373a7f9ac95f3e5455477466a3
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-02-25 11:05:26 -0300

    adding english version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,18 @@
-import { Socials } from '../';
 import './Footer.scss';
+import { useContext } from 'react';
+import { LanguageContext } from '../../contexts';
+import { Socials } from '../';
 
 export function Footer() {
+	const { language } = useContext(LanguageContext);
 	return (
 		<div className='footer'>
 			<Socials section='footer' />
 
 			<small className='footerText'></small>
 			<small className='footerText'>
-				2023 ©️ Matías Giménez - Built with{' '}
+				2023 ©️ Matías Giménez -{' '}
+				{language === 'spanish' ? 'Hecho con' : 'Built with'}{' '}
 				<a
 					href='https://es.reactjs.org/'
 					target='_blank'
