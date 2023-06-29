5850ccd12b8899611f9ae2daab5ba46d3fbc75af
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-06-29 18:13:55 -0300

    adding skills section

--- src/pages/SkillsPage.tsx
+++ src/pages/SkillsPage.tsx
@@ -1,8 +1,30 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState, useEffect } from 'react';
 import { MainLayout } from '../layout';
 import { Balancer } from 'react-wrap-balancer';
 
 export const SkillsPage: FunctionComponent = () => {
+	const [skills, setSkills] = useState([]);
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	const getData = (): void => {
+		fetch('../../data/skills.json', {
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				setSkills(data);
+				console.log(data);
+			});
+	};
+
 	return (
 		<MainLayout location='skills'>
 			<main className='flex flex-col items-center mt-16 min-h-60'>
@@ -12,12 +34,30 @@ export const SkillsPage: FunctionComponent = () => {
 					</h2>
 					I'm excited to share with you the different skills I've
 					acquired throughout my
-					<span className='font-light'>-still brief-</span> journey as
-					a software developer.
+					<span className='font-light'> -still brief-</span> journey
+					as a software developer.
 					<br /> With a passion for learning and a commitment to
 					excellence, I continually strive to expand my knowledge and
 					keep up with the ever-evolving world of technology.
 				</Balancer>
+				<section className='container flex mt-8 justify-center mb-4'>
+					<section className='grid py-4 grid-cols-2 w-90 bg-white-medium rounded-md md:grid-cols-3 xl:grid-cols-4 '>
+						{skills.map(({ name, alt, src }) => {
+							return (
+								<article className='flex py-4 px-4 gap-2 justify-center items-center text-center'>
+									<img
+										className='h-10 w-10 max-w-10'
+										src={src}
+										alt={alt}
+									/>
+									<span className='basis-9/12 text-left text-grey-darkest font-medium text-md'>
+										{name}
+									</span>
+								</article>
+							);
+						})}
+					</section>
+				</section>
 			</main>
 		</MainLayout>
 	);
