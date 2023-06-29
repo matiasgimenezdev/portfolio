5b9be7f40d6ae4dbc9fab390aeecf38c97be29cf
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-06-29 15:37:59 -0300

    adding menu for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent } from 'react';
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { FunctionComponent, useState } from 'react';
 import { PiMoonLight } from 'react-icons/pi';
 import { GrMenu } from 'react-icons/gr';
 
@@ -11,6 +12,13 @@ export const Navbar: FunctionComponent<NavbarProps> = ({
 	sections,
 	location,
 }) => {
+	const [hidden, setHidden] = useState(true);
+
+	const handleMenu = (): void => {
+		setHidden(!hidden);
+		console.log(hidden);
+	};
+
 	return (
 		<header className='h-20'>
 			<nav className='font-sans h-full shadow-md flex w-full items-center justify-between p-3'>
@@ -18,28 +26,37 @@ export const Navbar: FunctionComponent<NavbarProps> = ({
 					<a href='/'>Matias Gimenez</a>
 				</h1>
 
-				<ul className='flex align-center gap-8 '>
-					{sections.map((element: string) => {
-						return (
-							<li
-								className={
-									location.toLowerCase() === element
-										? 'text-white text-l drop-shadow-custom-grey'
-										: 'hidden text-grey-dark text-xl md:inline'
-								}
-								key={element}
-							>
-								{element}
-							</li>
-						);
-					})}
+				<ul className='flex align-center relative gap-8 md:flex-row'>
+					<div className='absolute top-14 -right-3 bg-white-medium border-l border-grey-dark w-60 text-center md:static md:bg-white md:border-none'>
+						{sections.map((element: string) => {
+							return (
+								<li
+									className={
+										location.toLowerCase() === element
+											? 'text-bold text-xl ml-3 drop-shadow-custom-grey md:text-white'
+											: `${
+													hidden ? 'hidden' : 'block'
+											  } text-grey-dark  flex justify-center items-center h-16 border-b border-grey-dark text-xl md:inline md:text-grey-dark md:ml-8 md:border-none`
+									}
+									key={element}
+								>
+									{element}
+								</li>
+							);
+						})}
+					</div>
 					<li>
 						{
 							<PiMoonLight className='text-3xl ml-10 text-grey-dark md:mr-4' />
 						}
 					</li>
 					<li>
-						{<GrMenu className='block text-3xl mr-4 md:hidden' />}
+						{
+							<GrMenu
+								className='block text-3xl mr-4 md:hidden'
+								onClick={handleMenu}
+							/>
+						}
 					</li>
 				</ul>
 			</nav>
