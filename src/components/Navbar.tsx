6ca8d76a3e43ac3964190cc827d6d8e793e3a726
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-06-29 11:27:32 -0300

    adding <header> tag

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,33 +12,37 @@ export const Navbar: FunctionComponent<NavbarProps> = ({
 	location,
 }) => {
 	return (
-		<nav className=' font-sans h-20 shadow-md flex w-full items-center justify-between p-3'>
-			<h1 className='text-grey-dark text-2xl font-medium'>
-				<a href='/'>Matias Gimenez</a>
-			</h1>
+		<header className='h-20'>
+			<nav className='font-sans h-full shadow-md flex w-full items-center justify-between p-3'>
+				<h1 className='text-grey-dark text-2xl ml-4 font-medium'>
+					<a href='/'>Matias Gimenez</a>
+				</h1>
 
-			<ul className='flex align-center gap-8'>
-				{sections.map((element: string) => {
-					return (
-						<li
-							className={
-								location.toLowerCase() === element
-									? 'text-white text-l drop-shadow-custom-grey'
-									: 'hidden text-grey-dark text-xl md:inline'
-							}
-							key={element}
-						>
-							{element}
-						</li>
-					);
-				})}
-				<li>
-					{
-						<PiMoonLight className='text-3xl ml-10 text-grey-dark md:mr-4' />
-					}
-				</li>
-				<li>{<GrMenu className='block text-3xl mr-4 md:hidden' />}</li>
-			</ul>
-		</nav>
+				<ul className='flex align-center gap-8 '>
+					{sections.map((element: string) => {
+						return (
+							<li
+								className={
+									location.toLowerCase() === element
+										? 'text-white text-l drop-shadow-custom-grey'
+										: 'hidden text-grey-dark text-xl md:inline'
+								}
+								key={element}
+							>
+								{element}
+							</li>
+						);
+					})}
+					<li>
+						{
+							<PiMoonLight className='text-3xl ml-10 text-grey-dark md:mr-4' />
+						}
+					</li>
+					<li>
+						{<GrMenu className='block text-3xl mr-4 md:hidden' />}
+					</li>
+				</ul>
+			</nav>
+		</header>
 	);
 };
