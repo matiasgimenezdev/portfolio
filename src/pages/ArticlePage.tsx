0666ae0b02dda697bb02ae43162cab801099f5e2
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-07-01 12:20:57 -0300

    adding article rendering

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,14 +1,41 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { MainLayout } from '../layout';
 import { Title } from '../components/Title';
+import { helpFetch } from '../helpers';
 
 export const ArticlePage: FunctionComponent = () => {
 	const { article } = useParams();
+	const [currentArticle, setCurrentArticle] = useState<Article>();
+	type Article = {
+		title: string;
+		date: string;
+		tags: string[];
+		path: string;
+	};
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data: Article[] = await helpFetch(
+					'../../public/data/articles.json'
+				);
+
+				const current: Article = data.filter((item) =>
+					item.path.includes(article ?? '')
+				)[0];
+				setCurrentArticle(current);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchData();
+	}, [article]);
+
 	return (
 		<MainLayout>
 			<main className='flex flex-col items-center mt-16 min-h-60'>
-				<Title title={article ?? ''} />
+				<Title title={currentArticle ? currentArticle.title : ''} />
 				<Link to='/blog' className='text-lg font-normal'>
 					Return to all the notes
 				</Link>
