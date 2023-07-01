7479aa7a89b6567b9e80b25e68d3bb1d694d7d2b
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-07-01 10:57:15 -0300

    changing transition duration

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -23,7 +23,7 @@ export const MainLayout: FunctionComponent<MainLayoutProps> = ({
 					initial={{ opacity: 0, x: -100 }}
 					animate={{ opacity: 1, x: 0 }}
 					exit={{ opacity: 0, x: 100 }}
-					transition={{ duration: 0.5 }}
+					transition={{ duration: 0.8 }}
 				>
 					{children}
 					<Footer />
