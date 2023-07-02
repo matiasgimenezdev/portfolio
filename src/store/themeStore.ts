df7aa573e3d676325bc9891c7ed12a0c71101485
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-07-02 17:59:57 -0300

    adding comment

--- src/store/themeStore.ts
+++ src/store/themeStore.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import { Theme } from '../types';
 
 export const useThemeStore = create<Theme>((set) => ({
+	//? Get last value from local storage?
 	theme: 'light',
 	updateTheme: (newTheme: string) => set({ theme: newTheme }),
 }));
