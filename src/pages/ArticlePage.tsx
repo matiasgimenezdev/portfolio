d621b66b04beb326af649e84ea62b1605b0ecfc6
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-07-04 11:45:27 -0300

    adding loader

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -6,14 +6,16 @@ import { Title } from '../components';
 import { Article } from '../types';
 import { useLanguageStore, useThemeStore } from '../store';
 import { articles } from '../data';
+import { Loader } from '../components';
 
 export const ArticlePage: FunctionComponent = () => {
 	const { article } = useParams();
 	const [currentArticle, setCurrentArticle] = useState<Article>();
+	const [articleContent, setArticleContent] = useState<string>('');
+	const [isLoading, setIsLoading] = useState(true);
+	const navigate = useNavigate();
 	const language = useLanguageStore((state) => state.language);
 	const updateLanguage = useLanguageStore((state) => state.updateLanguage);
-	const [articleContent, setArticleContent] = useState<string>('');
-	const navigate = useNavigate();
 	const theme = useThemeStore((state) => state.theme);
 
 	const handleLanguageSwitch = (): void => {
@@ -43,6 +45,7 @@ export const ArticlePage: FunctionComponent = () => {
 				}
 				const htmlContent = await response.text();
 				setArticleContent(htmlContent);
+				setIsLoading(false);
 			} catch (error) {
 				navigate('/error');
 			}
@@ -52,64 +55,74 @@ export const ArticlePage: FunctionComponent = () => {
 
 	return (
 		<MainLayout>
-			<main
-				className={`flex flex-col items-center pt-16 min-h-60 ${
-					theme == 'light'
-						? 'text-grey-dark bg-white'
-						: 'text-white bg-grey-darkest'
-				}`}
-			>
-				<button
-					className='w-7 h-7 text-center mb-3'
-					onClick={handleLanguageSwitch}
+			{isLoading ? (
+				<Loader />
+			) : (
+				<main
+					className={`flex flex-col items-center pt-16 min-h-60 ${
+						theme == 'light'
+							? 'text-grey-dark bg-white'
+							: 'text-white bg-grey-darkest'
+					}`}
 				>
-					{language === 'english' ? (
-						<img
-							src='/icons/english.svg'
-							alt='USA flag'
-							className='m-auto'
-						/>
-					) : (
-						<img
-							src='/icons/spanish.svg'
-							alt='Spain flag'
-							className='m-auto'
-						/>
-					)}
-				</button>
-				<p>{currentArticle?.fullDate}</p>
-				<Title title={currentArticle ? currentArticle?.title : ''} />
+					<button
+						className='w-7 h-7 text-center mb-3'
+						onClick={handleLanguageSwitch}
+					>
+						{language === 'english' ? (
+							<img
+								src='/icons/english.svg'
+								alt='USA flag'
+								className='m-auto'
+							/>
+						) : (
+							<img
+								src='/icons/spanish.svg'
+								alt='Spain flag'
+								className='m-auto'
+							/>
+						)}
+					</button>
+					<p>{currentArticle?.fullDate}</p>
+					<Title
+						title={currentArticle ? currentArticle?.title : ''}
+					/>
 
-				<ul>
-					{currentArticle?.tags.map((tag) => {
-						return (
-							<span
-								className='bg-grey-lightest py-1 px-3 ml-1 text-grey-darkest rounded-2xl text-xs border border-grey-base'
-								key={tag}
-							>
-								{tag}
-							</span>
-						);
-					})}
-				</ul>
+					<ul>
+						{currentArticle?.tags.map((tag) => {
+							return (
+								<span
+									className='bg-grey-lightest py-1 px-3 ml-1 text-grey-darkest rounded-2xl text-xs border border-grey-base'
+									key={tag}
+								>
+									{tag}
+								</span>
+							);
+						})}
+					</ul>
 
-				<p
-					dangerouslySetInnerHTML={{ __html: articleContent }}
-					className={`${
-						theme == 'light' ? 'text-grey-darkest' : 'text-white'
-					} text font-light mt-4 text-md w-[100%] xs:w-9/12 px-6 block py-2 md:w-[70%] md:text-lg`}
-				/>
+					<p
+						dangerouslySetInnerHTML={{ __html: articleContent }}
+						className={`${
+							theme == 'light'
+								? 'text-grey-darkest'
+								: 'text-white'
+						} text font-light mt-4 text-md w-[100%] xs:w-9/12 px-6 block py-2 md:w-[70%] md:text-lg`}
+					/>
 
-				<Link
-					to='/blog'
-					className={`text-md font-semibold ${
-						theme == 'light' ? 'text-grey-darkest' : 'text-white'
-					} mt-4 p-2 px-3 hover:-translate-x-5 transition duration-300 text-lg`}
-				>
-					<IoIosArrowRoundBack className='inline-block text-xl' />{' '}
-					Return to the blog
-				</Link>
-			</main>
+					<Link
+						to='/blog'
+						className={`text-md font-semibold ${
+							theme == 'light'
+								? 'text-grey-darkest'
+								: 'text-white'
+						} mt-4 p-2 px-3 hover:-translate-x-5 transition duration-300 text-lg`}
+					>
+						<IoIosArrowRoundBack className='inline-block text-xl' />{' '}
+						Return to the blog
+					</Link>
+				</main>
+			)}
 		</MainLayout>
 	);
 };
