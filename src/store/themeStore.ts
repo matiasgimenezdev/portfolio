e7a597f88aac5c48535a4fa06c928af14f347fff
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-09-02 19:05:36 -0300

    using prefered user theme

--- src/store/themeStore.ts
+++ src/store/themeStore.ts
@@ -1,9 +1,18 @@
 import { create } from 'zustand';
 import { Theme } from '../types';
 
-const theme = localStorage.getItem('preferedTheme');
+let theme: string = localStorage.getItem('preferedTheme') ?? '';
+
+if (!theme) {
+	const darkModeMediaQuery = window.matchMedia(
+		'(prefers-color-scheme: dark)'
+	);
+	theme = darkModeMediaQuery.matches ? 'dark' : 'light';
+}
 
 export const useThemeStore = create<Theme>((set) => ({
-	theme: theme ? theme : 'light',
+	theme: theme,
 	updateTheme: (newTheme: string) => set({ theme: newTheme }),
 }));
+
+console.log();
