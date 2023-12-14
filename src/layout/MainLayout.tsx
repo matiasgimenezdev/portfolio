662437e25e80789d90de0dc8f99b0a7b3bbc749d
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-12-14 19:02:02 -0300

    increase transition duration

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -31,7 +31,7 @@ export const MainLayout: FunctionComponent<MainLayoutProps> = ({
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
 					exit={{ opacity: 0 }}
-					transition={{ duration: 0.6 }}
+					transition={{ duration: 0.8 }}
 				>
 					{children}
 					<Footer />
