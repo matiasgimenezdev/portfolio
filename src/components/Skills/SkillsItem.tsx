0e44256b60d9829ba1b3a594b4a0346b7af18631
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-07-07 15:24:15 -0300

    fix: increase font-size

--- src/components/Skills/SkillsItem.tsx
+++ src/components/Skills/SkillsItem.tsx
@@ -17,7 +17,7 @@ export const SkillsItem: FunctionComponent<SkillsItemProps> = ({
 			className='flex py-3 px-1 gap-2 justify-center items-center text-center'
 		>
 			<img className='h-10 w-10 max-w-10' src={src} alt={alt} />
-			<span className='basis-9/12 text-left font-medium  text-xs sm:text-md'>
+			<span className='basis-9/12 text-left font-medium  text-[13px] sm:text-md'>
 				{name}
 			</span>
 		</article>
